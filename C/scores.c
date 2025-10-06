df3a1146cbd4aa346645f5c431ca802f9828689d
Kandyriii <pmugo2002@gmail.com>
2025-10-06 12:08:21 +0300

    array

---
+++ C/scores.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//constant
+const int N = 3;
+
+//prototype
+float average(int length, int array[]);
+
+int main(void)
+{
+    //get scores
+    int scores[N];
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+
+    //Print average
+    printf("Average: %f\n", average(N, scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) length;
+}
